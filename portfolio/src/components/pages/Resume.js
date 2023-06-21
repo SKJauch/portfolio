c62bc0cb9dd62f3fa092cc24c6ae460825370208
Stephanie Jauch <sjauch21@gmail.com>
2023-06-21 15:37:08 -0500

    resume

---
+++ portfolio/src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+        Here is a brief overview of my experience.
+      </p>
+    </div>
+  );
+}
