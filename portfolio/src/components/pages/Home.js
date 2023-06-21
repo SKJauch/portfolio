7160988df88e8c1d2040d27f3b5005f9e0bf5218
Stephanie Jauch <sjauch21@gmail.com>
2023-06-21 15:34:38 -0500

    home

---
+++ portfolio/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Wecome to my portfolio!  Please look around and contact me to work together!
+      </p>
+    </div>
+  );
+}
