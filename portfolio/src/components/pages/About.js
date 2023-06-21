464559cb53d47c08f8b37c3278187599515b1766
Stephanie Jauch <sjauch21@gmail.com>
2023-06-21 15:31:57 -0500

    About me

---
+++ portfolio/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am a full stack developer with education from the University of Texas at Austin and a background in Psychology and Law Enforcement.  I am 36 years old and travel around in my camper with my two cats, spending most of my time in Texas and Colorado.
+      </p>
+    </div>
+  );
+}
